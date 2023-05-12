8fd33803917f656d31d06c35ee383128f88c5344
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-05-12 12:35:19 +0100

    add styling to new activity form

--- src/components/Add-Activity.jsx
+++ src/components/Add-Activity.jsx
@@ -1,38 +1,67 @@
-import { Button, TextField } from "@mui/material"
-import { useState } from "react"
-import { postActivity } from "../utils/api"
-
+import { Button, Grid, TextField } from '@mui/material';
+import { useState } from 'react';
+import { postActivity } from '../utils/api';
 
 const AddActivity = (activities, setActivities) => {
-    const [newLocation, setNewLocation] = useState('')
-    const [newActivity, setNewActivity] = useState('')
-    const [newImage, setNewImage] = useState('')
+  const [newLocation, setNewLocation] = useState('');
+  const [newActivity, setNewActivity] = useState('');
+  const [newImage, setNewImage] = useState('');
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        postActivity(newLocation, newActivity, newImage).then(({data}) => {
-            setActivities([newLocation, newActivity, newImage, ...activities])
-        })
-        setNewActivity('')
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    postActivity(newLocation, newActivity, newImage).then(({ data }) => {
+      setActivities([newLocation, newActivity, newImage, ...activities]);
+    });
+    setNewActivity('');
+  };
 
-    return (
-        <section>
-            <form id="new-activity-form" onSubmit={handleSubmit}>
-                <TextField placeholder="Location" className="location-text" value={newLocation} onChange={(event) => {
-                    setNewLocation(event.target.value)
-                }}></TextField>
-                <TextField placeholder="Activity" className="activity-text" onSubmit={handleSubmit} value={newActivity} onChange={(event) => {
-                    setNewActivity(event.target.value)
-                }}></TextField>
-                <TextField placeholder="Image URL" className="image-url" onSubmit={handleSubmit} value={newImage} onChange={(event) => {
-                    setNewImage(event.target.value)
-                }}></TextField>
-                <Button type="submit" className="submit-button">Submit</Button>
+  return (
+    <Grid className="new-activity-form-grid">
+      <form className="new-activity-form" onSubmit={handleSubmit}>
+        <div className='new-location'>
+          <TextField
+            placeholder="Location"
+            className="location-input"
+            required
+            type='text'
+            value={newLocation}
+            onChange={(event) => {
+              setNewLocation(event.target.value);
+            }}
+          ></TextField>
+        </div>
+        <div className='new-activity'>
+          <TextField
+            placeholder="Activity"
+            className="activity-input"
+            required
+            multiline
+            autoComplete='off'
+            onSubmit={handleSubmit}
+            value={newActivity}
+            onChange={(event) => {
+              setNewActivity(event.target.value);
+            }}
+          ></TextField>
+        </div>
+        <div className='new-image-URL'>
+          <TextField
+            placeholder="Image URL"
+            className="image-URL"
+            required
+            onSubmit={handleSubmit}
+            value={newImage}
+            onChange={(event) => {
+              setNewImage(event.target.value);
+            }}
+          ></TextField>
+        </div>
+        <Button type="submit" className="submit-button" variant='contained'>
+          Submit
+        </Button>
+      </form>
+    </Grid>
+  );
+};
 
-            </form>
-        </section> 
-    )
-}
-
-export default AddActivity
+export default AddActivity;
